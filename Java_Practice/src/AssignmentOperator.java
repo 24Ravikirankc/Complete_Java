8417eb183692006baff6f9d7c3d668cb32749756
Ravikiran kc <ravikiran_24@yahoo.com>
2024-11-02 14:26:41 +0530

    update
    update

---
+++ Java_Practice/src/AssignmentOperator.java
@@ -0,0 +1,11 @@
+class AssignmentOperator {
+    public static void main(String[] args) {
+        int x = 29;
+        //x += 5; // x = x + 5 output is 10
+        //x -= 5; // x = x - 5 output is 0
+        // x *= 5; // x = x * 5 output is 25
+        // x /= 5; // x = x / 5 output is 1
+        x %= 13; // x = x % 13 output is 5 and
+        System.out.println(x);
+    }
+}
