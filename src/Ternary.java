6ac3cf704df0aeb47ec55e346720012b127f6c95
Ravikiran kc <ravikiran_24@yahoo.com>
2024-11-02 14:21:12 +0530

    update
    update

--- src/Ternary.java
+++
@@ -1,9 +0,0 @@
-class Ternary {
-    public static void main(String[] args) {
-        //int x = 8;
-       // System.out.println(++x); output is 9. ++x = 1 + x
-        int number = 0; /*  https://studyeasy.org/course-articles/s02l07-ternary-operator/  */
-        String result = (number > 0) ? "Positive" : (number < 0) ? "Negative" : "Zero";
-        System.out.println("The number is: " + result);
-    }
-}
