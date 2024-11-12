d25c15a37ac0c6d4906b42d8cac83c1c7a30febf
Ravikiran kc <ravikiran_24@yahoo.com>
2024-11-12 22:26:15 +0530

    updates
    updates

---
+++ Java_Practice/src/Classexample.java
@@ -0,0 +1,16 @@
+public class Classexample {
+    int x = 5;
+    int y = 6;
+
+    public static void main(String[] args) {
+
+        Classexample add = new Classexample();
+        Classexample sub = new Classexample();
+        //Main myObj2 = new Main();  // Object 2
+
+
+        System.out.println(add.x + add.y );
+        System.out.println(sub.x - sub.y);
+       // System.out.println(myObj2.x);
+    }
+}
--- Java_Practice/src/Switch.java
+++ Java_Practice/src/Switch.java
@@ -8,7 +8,7 @@ class Switch {
 //             case 2:
 //                 System.out.println(" x is 2");
 //                 break;
-//                 case 3:
+//               case 3:
 //                     System.out.println(" x is 3");
 //                     break;
 //                     default:
@@ -34,19 +34,60 @@ class Switch {
 //        }
 
 
-        String x = "B";
-        switch (x.toLowerCase()) { // ttoLowerCase key word helps to convert from caps to lower case and execute the programm
-            case "a":
-                            System.out.println(" x is a");
+//        String x = "B";
+//        switch (x.toLowerCase()) { // ttoLowerCase key word helps to convert from caps to lower case and execute the programm
+//            case "a":
+//                            System.out.println(" x is a");
+//                break;
+//            case "b":
+//                          System.out.println(" x is b");
+//                break;
+//            case "c":
+//                           System.out.println(" x is c");
+//                break;
+//            default:
+//                System.out.println(" x is non other than a, b, c");
+//        }
+//    }
+//}
+
+
+//The switch-case statement in Java is a control structure that allows you to execute one block of code among many alternatives based on the value of an expression. It serves as a cleaner and more readable alternative to multiple if-else statements when dealing with numerous conditions.
+//Syntax
+//The basic syntax of a switch-case statement is as follows:
+//java
+//switch (expression) {
+//        case value1:
+//        // code block for value1
+//        break;
+//        case value2:
+//        // code block for value2
+//        break;
+//// more cases...
+//default:
+//        // code block if no cases match
+//        }
+
+// Example
+
+        int n = 2;
+        switch (n) {
+            case 1:
+                System.out.println("Sunday");
                 break;
-            case "b":
-                          System.out.println(" x is b");
+            case 2:
+                System.out.println("Monday");
                 break;
-            case "c":
-                           System.out.println(" x is c");
-                break;
-            default:
-                System.out.println(" x is non other than a, b, c");
+            case 3:
+                System.out.println("Tuesday");
+            case 4:
+                System.out.println("Wednesday");
+            case 5:
+                System.out.println("Thursday");
+            case 6:
+                System.out.println("Friday");
+            case 7:
+                System.out.println("Saturday");
         }
     }
 }
