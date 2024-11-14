29242de482e7fc84a7342471682961f5684abf52
Ravikiran kc <ravikiran_24@yahoo.com>
2024-11-14 22:47:48 +0530

    updates
    updates

---
+++ Practices/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator
+{
+    int a;
+    public int Add(int x, int y)
+        {
+            a = x + y;
+            return a;
+        }
+}
+
+public class Add(String a[])
+{
+    int n1 = 2;
+    int n2 = 4;
+    Calculator Sum = new Calculator();
+    int r = Sum.Add(n1, n2);
+    System.out.println(r);
+}
